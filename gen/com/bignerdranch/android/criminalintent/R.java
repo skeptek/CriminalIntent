90175aaa71b18ccb01b88951820f206ba7bce257
skeptek <kforsythe76@gmail.com>
2014-06-01 11:01:02 -0400

    Added context menu for delete with multiselect support

--- gen/com/bignerdranch/android/criminalintent/R.java
+++ gen/com/bignerdranch/android/criminalintent/R.java
@@ -21,7 +21,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background_activated=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int add_crime=0x7f050006;
@@ -33,8 +34,9 @@ public final class R {
         public static final int crime_title=0x7f050003;
         public static final int dialog_date_datePicker=0x7f050002;
         public static final int fragmentContainer=0x7f050001;
-        public static final int menu_item_new_crime=0x7f05000a;
-        public static final int menu_item_show_subtitle=0x7f05000b;
+        public static final int menu_item_delete_crime=0x7f05000a;
+        public static final int menu_item_new_crime=0x7f05000b;
+        public static final int menu_item_show_subtitle=0x7f05000c;
         public static final int viewPager=0x7f050000;
     }
     public static final class layout {
@@ -45,7 +47,8 @@ public final class R {
         public static final int list_item_crime=0x7f030004;
     }
     public static final class menu {
-        public static final int fragment_crime_list=0x7f080000;
+        public static final int crime_list_item_context=0x7f080000;
+        public static final int fragment_crime_list=0x7f080001;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
